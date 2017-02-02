03dd93eaaab4e38f8abd7a18de05ab0749b67ae1
K2267 <K2267@student.jamk.fi>
2017-02-02 10:11:31 +0200

    Esimerkki

---
+++ PersonApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a one Student object with parametric constuctor
+            Teacher teacher = new Teacher("Kirsi", "Kernel", "D556");
+            teacher.Age = 35;
+            teacher.Address = "Piippukatu 2";
+            teacher.PhoneNumber = "010-243051";
+            Console.WriteLine(teacher.ToString());
+
+            teacher.TeacherMethod();
+            student.StudentMethod();
+        }
+    }
+}
---
+++ PersonApplication/Teacher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Teacher : Person
+    {
+        public string Room { get; set; }
+
+        // default constructor
+        public Teacher()
+        {
+        }
+
+        // parametric constuctor calls base class constructor with parameters
+        public Teacher(string firstName, string lastName, string room)
+            : base(firstName, lastName)
+        {
+            Room = room;
+        }
+
+        // just one method what Teacher can do 
+        public void TeacherMethod()
+        {
+            Console.WriteLine("This method belongs to Teacher!");
+        }
+
+        // override base class ToString()-method
+        public override string ToString()
+        {
+                //Person- luokan ToString + Room
+                //firstname + Lastname ... + Room
+                //Base viittaa yliluokkaan eli nyt Person-luokkaan
+            return base.ToString() + " " + Room;
+        }
+    }
+}
